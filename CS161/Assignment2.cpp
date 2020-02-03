bc0182018bce585b3836a94dbebdd92c326e6210
Zach <Zacharyat19@gmail.com>
2020-02-02 22:27:37 -0800

    update

--- CS161/Assignment2.cpp
+++ CS161/Assignment2.cpp
@@ -25,7 +25,7 @@ bool checkRange(int lowerBound, int upperBound,int testValue) {
  * * Post-Conditions: outputs true if letter is a capital
  * ************************************************************************/
 bool isCapital(char letter) {
-    if(letter >= 'A' || letter <= 'Z') {
+    if(letter >= 'A' && letter <= 'Z') {
         return true;
     }
     return false;
@@ -280,15 +280,205 @@ int getInt(std::string prompt) {
  * * Post-Conditions: outputs true or false
  * ************************************************************************/
 int main() {
-    std::cout << checkRange(10,20,15);
-    std::cout << isCapital('A');
-    std::cout << isEven(4);
-    std::cout << isOdd(3);
-    std::cout << equalityTest(3,2);
-    std::cout << isFloatEqual(2.0,3.5,0.1);
-    std::cout << isInt("1");
-    std::cout << numbersPresent("happ1ness");
-    std::cout << lettersPresent("123hi132");
+    std::cout << "\n--------------------------\tcheckRange()\t--------------------------\n";
+    std::cout << "\nRange: 0-5   Test Integer: -1\n";
+    std::cout << "Running checkRange(\"0,5,-1\")\n";
+    std::cout << "Expected: 0\t";
+    std::cout << "Actual: " << checkRange(0,5,-1) << std::endl;
+    (checkRange(0,5,-1)==false)? std::cout << "PASSED\n" : std::cout << "FAILED\n";
 
+    std::cout << "\nRange: 10-20   Test Integer: 12\n";
+    std::cout << "Running checkRange(\"10,20,12\")\n";
+    std::cout << "Expected: 1\t";
+    std::cout << "Actual: " << checkRange(10,20,12) << std::endl;
+    (checkRange(10,20,12)==true)? std::cout << "PASSED\n" : std::cout << "FAILED\n";
+
+    std::cout << "\n--------------------------\tisCapital()\t--------------------------\n";
+    std::cout << "\nTest Character: C\n";
+    std::cout << "Running isCapital(\'C\')\n";
+    std::cout << "Expected: 1\t";
+    std::cout << "Actual: " << isCapital('C') << std::endl;
+    (isCapital('C')==true)? std::cout << "PASSED\n" : std::cout << "FAILED\n";
+
+
+    std::cout << "\nTest Character: t\n";
+    std::cout << "Running isCapital(\'t\')\n";
+    std::cout << "Expected: 0\t";
+    std::cout << "Actual: " << isCapital('t') << std::endl;
+    (isCapital('t')==false)? std::cout << "PASSED\n" : std::cout << "FAILED\n";
+
+    std::cout << "\n--------------------------\tisEven()\t--------------------------\n";
+
+    std::cout << "\nTest Integer: 14\n";
+    std::cout << "Running isEven(14)\n";
+    std::cout << "Expected: 1\t";
+    std::cout << "Actual: " << isEven(14) << std::endl;
+    (isEven(14)==1)? std::cout << "PASSED\n" : std::cout << "FAILED\n";
+
+
+    std::cout << "\nTest Integer: -2\n";
+    std::cout << "Running isEven(-2)\n";
+    std::cout << "Expected: 1\t";
+    std::cout << "Actual: " << isEven(-2) << std::endl;
+    (isEven(-2)==1)? std::cout << "PASSED\n" : std::cout << "FAILED\n";
+
+    std::cout << "\n--------------------------\tisOdd()\t--------------------------\n";
+
+    std::cout << "\nTest Integer: 9\n";
+    std::cout << "Running isOdd(9)\n";
+    std::cout << "Expected: 1\t";
+    std::cout << "Actual: " << isOdd(9) << std::endl;
+    (isOdd(9)==1)? std::cout << "PASSED\n" : std::cout << "FAILED\n";
+
+
+    std::cout << "\nTest Integer: -49\n";
+    std::cout << "Running isOdd(-49)\n";
+    std::cout << "Expected: 1\t";
+    std::cout << "Actual: " << isOdd(-49) << std::endl;
+    (isOdd(-49)==1)? std::cout << "PASSED\n" : std::cout << "FAILED\n";
+
+    std::cout << "\n--------------------------\tequalityTest()\t--------------------------\n";
+
+    std::cout << "\nTest num1: 4   Test num2: 8\n";
+    std::cout << "Running equalityTest(4,8)\n";
+    std::cout << "Expected: -1\t";
+    std::cout << "Actual: " << equalityTest(4,8) << std::endl;
+    (equalityTest(4,8)==-1)? std::cout << "PASSED\n" : std::cout << "FAILED\n";
+
+
+    std::cout << "\nTest num1: -16   Test num2: 16\n";
+    std::cout << "Running equalityTest(16,-16)\n";
+    std::cout << "Expected: 1\t";
+    std::cout << "Actual: " << equalityTest(16,-16) << std::endl;
+    (equalityTest(16,-16)==1)? std::cout << "PASSED\n" : std::cout << "FAILED\n";
+
+    std::cout << "\n--------------------------\tisFloatEqual\t--------------------------\n";
+
+    std::cout << "\nTest num1: 4.2350   Test num2: 4.2300   Precision: 0.0001\n";
+    std::cout << "Running isFloatEqual(4.2350, 4.2300, 0.001)\n";
+    std::cout << "Expected: 0\t";
+    std::cout << "Actual: " << isFloatEqual(4.2350, 4.2300, 0.001) << std::endl;
+    (isFloatEqual(4.2350, 4.2300, 0.001)==0)? std::cout << "PASSED\n" : std::cout << "FAILED\n";
+
+
+    std::cout << "\nTest num1: 1.0000   Test num2: 1.0001   Precision: 0.001\n";
+    std::cout << "Running isFloatEqual(1.0000, 1.0001, 0.001)\n";
+    std::cout << "Expected: 1\t";
+    std::cout << "Actual: " << isFloatEqual(1.0000, 1.0001, 0.001) << std::endl;
+    (isFloatEqual(1.0000, 1.0001, 0.001)==1)? std::cout << "PASSED\n" : std::cout << "FAILED\n";
+
+    std::cout << "\n--------------------------\tisInt\t--------------------------\n";
+
+    std::cout << "\nTest string: \"hello\"\n";
+    std::cout << "Running isInt(\"hello\")\n";
+    std::cout << "Expected: 0\t";
+    std::cout << "Actual: " << isInt("hello") << std::endl;
+    (isInt("hello")==0)? std::cout << "PASSED\n" : std::cout << "FAILED\n";
+
+
+    std::cout << "\nTest string: \"8\"\n";
+    std::cout << "Running isInt(\"8\")\n";
+    std::cout << "Expected: 1\t";
+    std::cout << "Actual: " << isInt("8") << std::endl;
+    (isInt("8")==1)? std::cout << "PASSED\n" : std::cout << "FAILED\n";
+
+    std::cout << "\n--------------------------\tnumbersPresent()\t--------------------------\n";
+
+    std::cout << "\nTest string: \"Hello World\"\n";
+    std::cout << "Running numbersPresent(\"Hello World\")\n";
+    std::cout << "Expected: 0\t";
+    std::cout << "Actual: " << numbersPresent("Hello World") << std::endl;
+    (numbersPresent("Hello World")==0)? std::cout << "PASSED\n" : std::cout << "FAILED\n";
+
+
+    std::cout << "\nTest string: \"Hello World\"\n";
+    std::cout << "Running numbersPresent(\"I am 18 years old.\")\n";
+    std::cout << "Expected: 1\t";
+    std::cout << "Actual: " << numbersPresent("This is the number 12.") << std::endl;
+    (numbersPresent("I am 18 years old.")==1)? std::cout << "PASSED\n" : std::cout << "FAILED\n";
+
+    std::cout << "\n--------------------------\tlettersPresent()\t--------------------------\n";
+
+    std::cout << "\nTest string: \"11231]23[\"" << std::endl;
+    std::cout << "Running lettersPresent(\"11231]23[\"" << std::endl;
+    std::cout << "Expected: 0\t";
+    std::cout << "Actual: " << lettersPresent("15675431]23[") << std::endl;
+    (lettersPresent("11231]23[")==0)? std::cout << "PASSED\n" : std::cout << "FAILED\n";
+
+
+    std::cout << "\nTest string: \"1831809273k\"" << std::endl;
+    std::cout << "Running lettersPresent(\"1831809273k\")" << std::endl;
+    std::cout << "Expected: 1\t";
+    std::cout << "Actual: " << lettersPresent("1831809273k") << std::endl;
+    (lettersPresent("1831809273k")==1)? std::cout << "PASSED\n" : std::cout << "FAILED\n";
+
+    std::cout << "\n--------------------------\tcontainsSubString()\t--------------------------\n";
+
+    std::cout << "\nTest string: \"This is a test.\"   Test substring:\"this\"" << std::endl;
+    std::cout << "Running containsSubString(\"This is a test.\", \"test\")" << std::endl;
+    std::cout << "Expected: 1\t";
+    std::cout << "Actual: " << containsSubString("This is a test.","test") << std::endl;
+    (containsSubString("This is a test.","test")==1)? std::cout << "PASSED\n" : std::cout << "FAILED\n";
+
+
+    std::cout << "\nTest string: \"Is it done?\"   Test substring:\"year\"" << std::endl;
+    std::cout << "Running containsSubString(\"Is it done?\", \"year\")" << std::endl;
+    std::cout << "Expected: 0\t";
+    std::cout << "Actual: " << containsSubString("Is it still 2019?","year") << std::endl;
+    (containsSubString("Is it still year?","year")==0)? std::cout << "PASSED\n" : std::cout << "FAILED\n";
+
+    std::cout << "\n--------------------------\twordCount()\t--------------------------\n";
+
+    std::cout << "\nTest sentence: \"Hello, this is a test.\"" << std::endl;
+    std::cout << "Running wordCount(\"Hello, this is a test.\")" << std::endl;
+    std::cout << "Expected: 5\t";
+    std::cout << "Actual: " << wordCount("Hello, my name is Akhil.") << std::endl;
+    (wordCount("Hello, this is a test.")==5)? std::cout << "PASSED\n" : std::cout << "FAILED\n";
+
+
+    std::cout << "\nTest sentence: \"Insert something important\"" << std::endl;
+    std::cout << "Running wordCount(\"Insert something important!\")" << std::endl;
+    std::cout << "Expected: 3\t";
+    std::cout << "Actual: " << wordCount("Insert something here") << std::endl;
+    (wordCount("Hydration is Important!")==3)? std::cout << "PASSED\n" : std::cout << "FAILED\n";
+
+    std::cout << "\n--------------------------\ttoUpper()\t--------------------------\n";
+
+    std::cout << "\nTest sentence: \"I Like Dogs!\"" << std::endl;
+    std::cout << "Running toUpper(\"I Like Dogs!\")" << std::endl;
+    std::cout << "Expected: I LIKE DOGS!\t";
+    std::cout << "Actual: " << toUpper("I Like Dogs!") << std::endl;
+    (toUpper("I Like Dogs!") == ("I LIKE DOGS!"))? std::cout << "PASSED\n" : std::cout << "FAILED\n";
+
+
+    std::cout << "\nTest sentence: \"pizza\"" << std::endl;
+    std::cout << "Running toUpper(\"pizza\")" << std::endl;
+    std::cout << "Expected: PIZZA\t";
+    std::cout << "Actual: " << toUpper("pizza") << std::endl;
+    (toUpper("pizza") == ("PIZZA"))? std::cout << "PASSED\n" : std::cout << "FAILED\n";
+
+    std::cout << "\n--------------------------\ttoLower()\t--------------------------\n";
+
+    std::cout << "\nTest sentence: \"GO BEAVS!\"" << std::endl;
+    std::cout << "Running toLower(\"GO BEAVS!\")" << std::endl;
+    std::cout << "Expected: go beavs!\t";
+    std::cout << "Actual: " << toLower("GO BEAVS!") << std::endl;
+    (toLower("GO BEAVS!") == ("go beavs!"))? std::cout << "PASSED\n" : std::cout << "FAILED\n";
+
+
+    std::cout << "\nTest sentence: \"RANDOMWORDS123\"" << std::endl;
+    std::cout << "Running toLower(\"RANDOMWORDS123\")" << std::endl;
+    std::cout << "Expected: randomwords123\t";
+    std::cout << "Actual: " << toLower("RANDOMWORDS123") << std::endl;
+    (toLower("RANDOMWORDS123") == ("randomwords123"))? std::cout << "PASSED\n" : std::cout << "FAILED\n";
+
+    std::cout << "\n--------------------------\tgetInt()\t--------------------------\n";
+
+    std::string user_input = "";
+    std::cout << "Running get_int(int)" << std::endl;
+    std::cout << "Enter a number: ";
+    std::getline(std::cin, user_input);
+    std::cout << "\nYour number is " << getInt(user_input) << "\n\n";
+    
     return 0;
 }
