354b8ba5bda1e76ac93cd4c8535d8eaede7276b8
Zach Taylor <44819790+Zacharyat19@users.noreply.github.com>
2025-05-28 17:10:19 -0700

    Update vector.cpp

--- C++_Projects/vector.cpp
+++ C++_Projects/vector.cpp
@@ -4,11 +4,6 @@
 
 using std::exception;
 
-//We do not want to include either stmt. We wouldn't 
-//be able to compare our vector template to the Standard 
-//using namespace std;
-//using std::vector;
-
 int main (){
    vector<int> v;   //Our vector class
    std::vector<int> stdv; //Standard vector
